a5c0e0fa2101d381b10863d474a1e4b12bceac24
Viresh <90565006+Vireshkamlapure@users.noreply.github.com>
2025-07-07 09:17:59 +0530

    Add files via upload
    Built a GUI based library app for Book Management using J2EE(Java servlet) and JDBC to perform CRUD operations using Web servlet.

---
+++ src/MiniProject/Book_App/src/main/java/com/dao/BookDAO.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import com.model.Book;
+
+import java.util.List;
+public interface BookDAO {
+    int insert(Book book);
+    int update(Book book);
+    int delete(Book book);
+
+    List<Book> view();
+    Book view(int id);
+    Book view(String name);
+}
